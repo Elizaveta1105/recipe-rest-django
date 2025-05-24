ee60121666916e3fe5046229f996c649b5b64312
Yelyzaveta <elizaveta@short.io>
2025-05-24 09:41:23 +0300

    user profile tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,6 +6,8 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -54,3 +56,99 @@ class PublicUserApiTests(TestCase):
         user_exists = get_user_model().objects.filter(email=payload['email']).exists()
         self.assertFalse(user_exists)
 
+
+    def test_create_token_for_user(self):
+        """Test generates token for valid creds"""
+        user_details = {
+            'name': 'Test name',
+            'email': 'test@example.com',
+            'password': 'test-user-password123'
+        }
+        create_user(**user_details)
+
+        payload = {
+            'email': user_details['email'],
+            'password': user_details['password']
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+    def test_create_token_bad_creds(self):
+        """Error if creds invalid"""
+        user_details = {
+            'name': 'Test name',
+            'email': 'test@example.com',
+            'password': 'goodpass'
+        }
+        create_user(**user_details)
+
+        payload = {
+            'email': user_details['email'],
+            'password': 'badpass'
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_create_token_blank_pass(self):
+        """Error if blank password"""
+        payload = {
+            'email': '',
+            'password': ' '
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_retrieve_user_unauthorized(self):
+        """Test auth is required for user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API request that require authentication"""
+    def setUp(self):
+        self.user = create_user(email='test@example.com', password='testpass123', name='Testname')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for a user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'email': self.user.email,
+            'name': self.user.name
+        })
+
+    def test_post_me_not_allowed(self):
+        """Post is not allowed for the me endpoint"""
+
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating user profile for the authed user"""
+
+        payload = {
+            'name': 'Updated name',
+            'password': 'newpass123'
+        }
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,9 @@
 """
 Views for the user API
 """
-
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from . import serializers
 
@@ -10,3 +11,9 @@ from . import serializers
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = serializers.UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create auth token for user"""
+    serializer_class = serializers.AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
